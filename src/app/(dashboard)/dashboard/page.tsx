1a885f28c0a9735e4ff633034102febf500ea7aa
Neeraj Dalal <admin@nrjdalal.com>
2023-05-06 23:00:52 +0530

    feat: added dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 
-export default function Home() {
+import { UserButton } from '@clerk/nextjs'
+
+export default function Page() {
   return (
     <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Link href="/dashboard">Sign In</Link>
+      <UserButton />
     </div>
   )
 }
