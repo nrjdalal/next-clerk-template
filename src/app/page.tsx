da28ddca443e0037253517195a0d0993031593e1
Neeraj Dalal <admin@nrjdalal.com>
2023-05-06 21:06:20 +0530

    feat: disable auto redirect to sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div className="fixed right-10 top-10">
-      <UserButton />
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <SignedOut>
+        <Link href="sign-in">Please Sign In To Continue</Link>
+      </SignedOut>
     </div>
   )
 }
