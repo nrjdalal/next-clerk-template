cd89c1807de2426184492ca1aa47c882f8fc70b5
Neeraj Dalal <admin@nrjdalal.com>
2023-05-10 10:36:33 +0530

    test: test alternate sign-in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Link href="/dashboard">Sign In</Link>
+      <Link href="/sign-in">Sign In</Link>
     </div>
   )
 }
