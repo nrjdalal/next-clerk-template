16fd6f81fe39d1df4df458995de6e76bc38ac8b1
Neeraj Dalal <admin@nrjdalal.com>
2023-05-11 08:49:47 +0530

    test: line 1\nline2

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider } from '@clerk/nextjs/app-beta'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <ClerkProvider>{children}</ClerkProvider>
